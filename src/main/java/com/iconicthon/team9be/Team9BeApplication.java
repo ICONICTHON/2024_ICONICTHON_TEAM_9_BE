bab381e7475aad44184d764d0a61fac83856597e
Limwngur <ovg07047@naver.com>
2024-11-14 16:46:16 +0900

    initial commit

---
+++ src/main/java/com/iconicthon/team9be/Team9BeApplication.java
@@ -0,0 +1,13 @@
+package com.iconicthon.team9be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team9BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team9BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iconicthon/team9be/Team9BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iconicthon.team9be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team9BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
